b5783b962207a452300729170f4e77e608508313
idris-adigun <idris.adigun@ymail.com>
2024-12-27 18:19:16 -0500

    Remove README.md and update setup.py to read long description from README

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="dynamic_cli_builder",
-    version="0.1.4",
+    version="0.1.5",
     description="A Python library for dynamically building CLI tools from YAML configurations.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Idris Adigun",
     author_email="adigun.idris@ymail.com",
     url="https://github.com/idris-adigun/dynamic-cli-builder",
