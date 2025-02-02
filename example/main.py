36becddd749ccc2cc751ba38a52ad819de0eefe1
Idris Adigun <edijman@gmail.com>
2025-02-01 23:29:53 -0500

    Update main.py to use JSON config and add initial JSON configuration file

--- example/main.py
+++ example/main.py
@@ -2,5 +2,5 @@ from dynamic_cli_builder import run_builder
 from actions import ACTIONS
 
 if __name__ == '__main__':
-    run_builder('config.yaml', ACTIONS)
+    run_builder('config.json', ACTIONS)
 
